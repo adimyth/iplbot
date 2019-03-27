53dc854d1732587c9419948309ff334f6ad287b6
adimyth <mishraaditya6991@gmail.com>
2019-03-28 00:46:18 +0530

    adds fakeuseragent

--- scraper.py
+++ scraper.py
@@ -3,24 +3,24 @@ import requests
 import json
 import pandas as pd
 from pprint import pprint
+from fake_useragent import UserAgent
 
 
 def get_points_table():
+    ua = UserAgent()
+    header = {'User-Agent': str(ua.chrome)}
     url = 'https://www.cricbuzz.com/cricket-series/2810/indian-premier-league-2019/points-table'
-    response = requests.get(url)
+    response = requests.get(url, headers=header)
     soup = BeautifulSoup(response.content, 'html.parser')
-
     data = []
     final_data = []
     points_table = soup.find("table", {"class": "table cb-srs-pnts"})
     table_body = points_table.find('tbody')
-
     rows = table_body.find_all('tr')
     for row in rows:
         cols = row.find_all('td')
         cols = [ele.text.strip() for ele in cols]
         data.append([ele for ele in cols if ele])
-
     response_dict = {}
     count = 0
     for x in data:
@@ -37,3 +37,7 @@ def get_points_table():
             temp['nrr'] = x[7]
             response_dict[count] = temp
     return response_dict
+
+
+if __name__ == '__main__':
+    print(get_points_table())
