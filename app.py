96af04b9af479dbc71f52b45d08cbe38761f4c76
adimyth <mishraaditya6991@gmail.com>
2020-08-20 14:30:12 +0530

    modifies template

--- app.py
+++ app.py
@@ -11,15 +11,40 @@ st.markdown(
 )
 
 # get input text
-input_text = st.text_input(label="English text.")
+st.subheader("Query")
+input_text = st.text_input(label="")
 
 # return response
 if st.button("Submit"):
     with st.spinner("Thinking ..."):
         response = generate_response(input_text)
-        st.subheader("Predicted Spanish Translation")
+        st.subheader("Botvic Says -")
         st.success(f"{response}")
 
 st.markdown(
-    "The interactive app is created using [Streamlit](https://streamlit.io/), an open-source framework that lets users creating apps for machine learning projects very easily."
+    "The interactive app is created using [Streamlit](https://streamlit.io/), an open-source framework that lets users creating apps for machine learning projects very easily. GitHub Repo - [iplbot](https://github.com/adimyth/iplbot)."
 )
+
+st.sidebar.title("About")
+st.sidebar.info(
+    "Retrieval Based Chatbot\n\n"
+    "Check the code at https://github.com/adimyth/iplbot"
+)
+
+def load_css():
+    st.markdown('<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">', unsafe_allow_html=True)
+
+def display_icons(icon, link, name=""):
+    if name=="":
+        name = icon.capitalize()
+    return st.sidebar.markdown(f'<a href="{link}"><i class="fa fa-{icon} fa-fw"></i>&nbsp; {name}</a>', unsafe_allow_html=True)    
+
+def display_media(icon, link):
+    return st.sidebar.markdown(f'<a href="{link}"><i class="fa fa-{icon}-square fa-fw"></i>&nbsp; {icon.capitalize()}</a>', unsafe_allow_html=True)
+
+st.sidebar.title("Contact Me")
+load_css()
+display_icons("globe", "https://adimyth.github.io/notes/", "Blog")
+display_media("twitter", "https://twitter.com/adi_myth")
+display_media("github", "https://github.com/adimyth")
+display_media("linkedin", "https://www.linkedin.com/in/aditya-mishra-b50623138")
